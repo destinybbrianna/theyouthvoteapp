cedc65454fd603f3faa468a3d25fb8428ccd0d5d
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-15 13:08:56 -0700

    Iteration 2

---
+++ THE YOUTH VOTE PART 2/RegisterViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RegisterViewController.swift
+//  THE YOUTH VOTE PART 2
+//
+//  Created by Apple on 8/15/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+
+    @IBOutlet weak var typeTexthere: UITextField!
+
+
+    @IBOutlet weak var typePasswordHere: UITextField!
+
+
+    @IBOutlet weak var typeAgeHere: UITextField!
+
+
+    @IBAction func submitButton(_ sender: UIButton) {
+    }
+//    if let typeTexthere.text == "controlVersey" {
+//            textAppersHere.text = "Welcome to The Youth Vote!"
+//   else if typeTexthere.text != "controlVersey" {
+//            textAppearsHere.text = "Incorrect Password"
+//    }
+//   textAppearsHere.text = thereIsText
+////            if typeTextHere.text == "password" {
+////                typeAppearsHere.text = "Welcome"
+////            }
+////    }
+////        else if typeTextHere.text != "password" {
+////        textAppearsHere.text = "Incorrect"
+////        }
+////
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+////        // Do any additional setup after loading the view.
+////    }
+////}
+////
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
